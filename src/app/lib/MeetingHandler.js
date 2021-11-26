74ff7010891f9279e45bc9a53fa00512c7ba9a9b
Ajand Mardalizad <aseedgeek@gmail.com>
2021-11-26 16:51:19 +0330

    protoo Server connected

--- src/app/lib/MeetingHandler.js
+++ src/app/lib/MeetingHandler.js
@@ -1,4 +1,4 @@
-import peer from './protooClient'
+import peer from "./protooClient";
 
 /**
  * Room Data Structure
@@ -14,53 +14,59 @@ import peer from './protooClient'
  */
 
 export const CreateRoom = () => {
-    // Will Create A room, 
-    // Only Moderators could do it
-    //  Will notify Others that the room is created
+  // Will Create A room,
+  // Only Moderators could do it
+  //  Will notify Others that the room is created
 
-    console.log(peer.connected, peer.closed, peer.data)
-}
+  console.log(peer.connected, peer.closed, peer.data);
+  peer
+    .request("createRoom", { roomName: "here we are" })
+    .then((d) => {
+      console.log(d, 'create room response');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 export const JoinRoom = () => {
-    // Will join to a room that is created
-    // If the room does not exist will throw an error
-}
+  // Will join to a room that is created
+  // If the room does not exist will throw an error
+};
 
 export const ExitRoom = () => {
-    // Will exit the room
-    // If it's not in the room it will do nothing
-}
+  // Will exit the room
+  // If it's not in the room it will do nothing
+};
 
 export const JoinStage = () => {
-    // Will join the stage
-    // It could be configurable -> Only moderator could do it | Speakers can also do it
-}
+  // Will join the stage
+  // It could be configurable -> Only moderator could do it | Speakers can also do it
+};
 
 export const LeaveStage = () => {
-    // Will leave the stage
-    // Moderators could use it for anyone
-    // Others could use it for themselves
-}
+  // Will leave the stage
+  // Moderators could use it for anyone
+  // Others could use it for themselves
+};
 
 export const RaiseHand = () => {
-    // Will notify moderators that someone wants to come to stage
-    // Only if there is no raise hand yet
-}
+  // Will notify moderators that someone wants to come to stage
+  // Only if there is no raise hand yet
+};
 
 export const TurnOffAudio = () => {
-    // Will turn off the audio of stager
-}
+  // Will turn off the audio of stager
+};
 
 export const TurnOnAudio = () => {
-    // Will turn on the audio of stager
-}
+  // Will turn on the audio of stager
+};
 
 export const TurnOffVideo = () => {
-    // Will turn off the video of the stager
-}
+  // Will turn off the video of the stager
+};
 
 export const TurnOnVideo = () => {
-    // Will turn on the video of the stager
-}
-
-
+  // Will turn on the video of the stager
+};
--- src/app/lib/protooClient.js
+++ src/app/lib/protooClient.js
@@ -1,10 +1,12 @@
 import protooClient from "protoo-client";
 
 const transport = new protooClient.WebSocketTransport(
-  process.env.REACT_APP_PROTOO
+  `${process.env.REACT_APP_PROTOO}/?roomId=123&username=heyvooni`
 );
 
 const peer = new protooClient.Peer(transport);
+
+
 
 peer.on("open", () => {
   console.log("peer is open");
@@ -22,8 +24,8 @@ peer.on("close", () => {
   console.log("peer has close");
 });
 
-peer.on("notification", () => {
-  console.log("peer has notification");
+peer.on("notification", (notif) => {
+  console.log("peer has notification", notif);
 });
 
 peer.on("request", () => {
